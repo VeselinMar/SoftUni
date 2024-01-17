92771527135b27d7959cf653a4d9e01139ca1f87
VeselinMar <veselin.martinov@gmail.com>
2024-01-17 13:20:25 +0100

    solutions from stacks and queues uploaded

--- advanced/stacks_and_queues/fast_food.py
+++ advanced/stacks_and_queues/fast_food.py
@@ -2,21 +2,19 @@
 from collections import deque
 # quantity of food for the day
 quantity_of_food = int(input())
-# save orders into a queue and make a second queue from which to take the biggest order
+# save orders into a queue
 orders = input().split()
 orders_queue = deque()
-biggest_order = deque()
+biggest_order = 0
 for order in orders:
     orders_queue.append(int(order))
     if biggest_order:
-        if int(order) > biggest_order[-1]:
-            biggest_order.append(int(order))
-        else:
-            biggest_order.appendleft(int(order))
+        if int(order) > biggest_order:
+            biggest_order = int(order)
     else:
-        biggest_order.append(int(order))
+        biggest_order = int(order)
 # print biggest order
-print(biggest_order[-1])
+print(biggest_order)
 # complete orders if quantity is enough
 while orders_queue:
     if quantity_of_food >= orders_queue[0]:
