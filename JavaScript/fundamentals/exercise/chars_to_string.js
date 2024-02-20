4872e0206e07b369fa0b7c22aedad7898c541686
Veselin Martionv <veselin.martinov@gmail.com>
2024-02-20 12:47:25 +0100

    js update

--- JavaScript/fundamentals/exercise/chars_to_string.js
+++ JavaScript/fundamentals/exercise/chars_to_string.js
@@ -2,7 +2,6 @@ function chars_to_string(first, second, third) {
     let result = ""
     result += first + second + third
     console.log(result)
-    console.log(typeof result)
 }
 
 chars_to_string("a", "b", "c")
---
+++ JavaScript/fundamentals/exercise/reversed_chars.js
@@ -0,0 +1,8 @@
+function reverse_chars(first, second, third) {
+    let result = ""
+    result += third + " " + second + " " + first
+    console.log(result)
+}
+
+reverse_chars("A", "B", "C")
+reverse_chars("1", "L", "&")
