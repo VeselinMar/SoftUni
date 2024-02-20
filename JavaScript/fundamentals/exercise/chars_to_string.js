6ae883385f619d55d1bc4c853c210f30d6d64824
Veselin Martionv <veselin.martinov@gmail.com>
2024-02-20 12:43:23 +0100

    js update

---
+++ JavaScript/fundamentals/exercise/chars_to_string.js
@@ -0,0 +1,10 @@
+function chars_to_string(first, second, third) {
+    let result = ""
+    result += first + second + third
+    console.log(result)
+    console.log(typeof result)
+}
+
+chars_to_string("a", "b", "c")
+chars_to_string("%", "2", "0")
+chars_to_string("1", "5", "p")
