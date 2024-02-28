759135ce9f5af63b229020be54db9367d9fe205d
Veselin Martionv <veselin.martinov@gmail.com>
2024-02-28 14:22:24 +0100

    python up

---
+++ Python/oop/Inheritance/lab/Animals/cat.py
@@ -0,0 +1,7 @@
+from Python.oop.Inheritance.lab.Animals.animal import Animal
+
+
+class Cat(Animal):
+
+    def meow(self):
+        return "meowing..."
---
+++ Python/oop/Inheritance/lab/Animals/dog.py
@@ -0,0 +1,7 @@
+from Python.oop.Inheritance.lab.Animals.animal import Animal
+
+
+class Dog(Animal):
+
+    def bark(self):
+        return "barking..."
