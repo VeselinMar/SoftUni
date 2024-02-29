65da8da3656bed5733671d4d396880a5cc761fd3
Veselin Martionv <veselin.martinov@gmail.com>
2024-02-29 19:35:20 +0100

    python classes inheritance update

---
+++ Python/oop/Inheritance/exercise/Zoo/lizard.py
@@ -0,0 +1,7 @@
+from Python.oop.Inheritance.exercise.Zoo.reptile import Reptile
+
+
+class Lizard(Reptile):
+
+    def __init__(self, name):
+        super().__init__(name)
---
+++ Python/oop/Inheritance/exercise/Zoo/reptile.py
@@ -0,0 +1,7 @@
+from Python.oop.Inheritance.exercise.Zoo.animal import Animal
+
+
+class Reptile(Animal):
+
+    def __init__(self, name):
+        super().__init__(name)
