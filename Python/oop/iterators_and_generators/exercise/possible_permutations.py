9eb6570647144c571afbf6ff5bc33d8e64bb9cc4
Veselin Martionv <veselin.martinov@gmail.com>
2024-03-18 20:03:29 +0100

    python iterators and generators up

--- Python/oop/iterators_and_generators/exercise/possible_permutations.py
+++ Python/oop/iterators_and_generators/exercise/possible_permutations.py
@@ -1,17 +1,11 @@
-from collections import deque
-
 def possible_permutations(list_input: list):
     def dfs(elements, path):
         if not elements:
             yield path
-        seen = set()
         for i, element in enumerate(elements):
-            if element not in seen:
-                seen.add(element)
+            if element not in path:
                 yield from dfs(elements[:i] + elements[i + 1:], path + [element])
 
-    list_input = list(list_input)  # Convert deque to list for slicing
-    elements = len(list_input)
     for permutation in dfs(list_input, []):
         yield permutation
 
--- Python/oop/iterators_and_generators/exercise/reader.py
+++ Python/oop/iterators_and_generators/exercise/reader.py
@@ -1,5 +1,10 @@
-def read_next():
-    pass
+def read_next(*items):
+    for item in items:
+        if isinstance(item, dict):
+            for key in item.keys():
+                yield key
+        for letter in item:
+            yield letter
 
 
 for item in read_next("string", (2,), {"d": 1, "i": 2, "c": 3, "t": 4}):
