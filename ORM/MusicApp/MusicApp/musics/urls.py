f9f159790f01f9b9870af8898395e5c278b71e6a
Veselin Martionv <veselin.martinov@gmail.com>
2024-08-11 20:04:42 +0200

    applied html inheritance and added session_decorator.py

--- ORM/MusicApp/MusicApp/musics/urls.py
+++ ORM/MusicApp/MusicApp/musics/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from MusicApp.musics import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('MusicApp.musics.urls')),
+    path('', views.index, name='index'),
+    path('album/', include([
+        path('create/', views.create_album, name='create album'),
+        path('details/<int:pk>/', views.album_details, name='details album'),
+        path('edit/<int:pk>/', views.edit_album, name='edit album'),
+        path('delete/<int:pk>/', views.delete_album, name='delete album')
+    ])),
+    path('song/', include([
+        path('create/', views.create_song, name='create songs')
+    ])),
 ]
